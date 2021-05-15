2a1cdfdebe522139e8cf37e8fe087b94115ddd9b
codergo7 <codergo7@gmail.com>
2021-05-15 06:15:55 +0200

    added Vet list and updated VetController. Closes #14

--- pet-clinic-web/src/main/java/com.go.springpetclinic/cobtroller/VetController.java
+++ pet-clinic-web/src/main/java/com.go.springpetclinic/cobtroller/VetController.java
@@ -1,14 +1,22 @@
 package com.go.springpetclinic.cobtroller;
 
+import com.go.springpetclinic.service.VetService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/vets")
+@RequiredArgsConstructor
 public class VetController {
 
-    @RequestMapping({"/vets","vets/index", "vets/index.html"})
-    public String listVets(){
+    private final VetService vetService;
+
+    @GetMapping({"","/index", "/index.html"})
+    public String listVets(Model model){
+        model.addAttribute("vets",vetService.findAll());
         return "vets/index";
     }
 }
