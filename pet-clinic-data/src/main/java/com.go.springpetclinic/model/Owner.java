add3cd375758305c93046b8b710475314659f3ac
codergo7 <codergo7@gmail.com>
2021-05-19 09:14:13 +0200

    added contact info to Owner. Closes #26

--- pet-clinic-data/src/main/java/com.go.springpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com.go.springpetclinic/model/Owner.java
@@ -1,8 +1,17 @@
 package com.go.springpetclinic.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Set;
 
+
+@Getter
+@Setter
 public class Owner extends Person{
 
+    private String address;
+    private String city;
+    private String telephone;
     private Set<Pet> pets;
 }
