8b5402ba6aec164e7083606c60d1abd2aa07a501
codergo7 <codergo7@gmail.com>
2021-05-25 11:50:10 +0200

    converted Owner, Pet and PetType to JPA entities . closes #35

--- pet-clinic-data/src/main/java/com.go.springpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com.go.springpetclinic/model/Owner.java
@@ -3,10 +3,15 @@ package com.go.springpetclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.util.HashSet;
 import java.util.Set;
 
-
+@Entity
+@Table(name = "owners")
 @Getter
 @Setter
 public class Owner extends Person{
@@ -14,5 +19,7 @@ public class Owner extends Person{
     private String address;
     private String city;
     private String telephone;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 }
--- pet-clinic-data/src/main/java/com.go.springpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com.go.springpetclinic/model/PetType.java
@@ -3,6 +3,11 @@ package com.go.springpetclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "types")
 @Getter
 @Setter
 public class PetType extends BaseEntity{
