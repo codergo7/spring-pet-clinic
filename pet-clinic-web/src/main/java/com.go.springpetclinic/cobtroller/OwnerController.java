b18630e5b3622fef0113133999ba1b622ca2e81b
codergo7 <codergo7@gmail.com>
2021-05-25 11:28:57 +0200

    fixed the bugs about find and error pages. closes #44

--- pet-clinic-web/src/main/java/com.go.springpetclinic/cobtroller/OwnerController.java
+++ pet-clinic-web/src/main/java/com.go.springpetclinic/cobtroller/OwnerController.java
@@ -4,6 +4,7 @@ import com.go.springpetclinic.service.OwnerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,5 +19,11 @@ public class OwnerController {
 
         model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
+    }
+
+    @GetMapping("/find")
+    public String findOwners(){
+
+        return "notimplemented";
     }
 }
